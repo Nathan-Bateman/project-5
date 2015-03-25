20ab8db470725f676c69d6970882cb397a1de57e
Nathan Bateman <nathan.bateman.jr@gmail.com>
2015-03-25 16:11:49 +0700

    added knockout observable array and new person function not yet working or functioning...investigate why

--- js/neighborhood.js
+++ js/neighborhood.js
@@ -1,9 +1,53 @@
-function initialize() {
+/*function initialize() {
 	var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: { lat: 13.66448, lng: 100.66160},
+          zoom: 16
         };
+    var latLong = new google.maps.LatLng(13.66448,100.66160);
     var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
-      }
+      };
+    var marker = new google.maps.Marker({
+      		position: latLong,
+      		map: map,
+      		title: 'Hello World!'
+  });
+
+
       google.maps.event.addDomListener(window, 'load', initialize);
+*/
+function initialize() {
+  var myLatlng = new google.maps.LatLng(13.66448,100.66160);
+  var mapOptions = {
+    zoom: 16,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function Person (name, title, yearshere) {
+ this.name = ko.observable(name);
+ this.title = ko.observable(title);
+ this.yearshere = ko.observable(yearshere);
+
+ }
+
+function mapViewModel() {
+	var self = this;
+	self.people = ko.observableArray([
+ new Person("Tony", "HS Teacher - Computers", 4),
+ new Person("Allan J", "MS Teacher - Math", 6)
+ ]);
+
+
+
+};
+ko.applyBindings(new mapViewModel());
